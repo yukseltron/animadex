ca01c19dd53b6a05bc9dfe5c783110fbddda9509
Viraj Bangari <Viraj3f@gmail.com>
2016-03-05 18:28:22 -0500

    take surl through python server

--- web.py
+++ web.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from wiki import WikiParse
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Beware of jeff"
+
+@app.route("/find")
+def find():
+    w = WikiParse()
+    inputUrl = input("url")
+    
+    matches = w.get_matches(inputUrl)
+    return matches
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
