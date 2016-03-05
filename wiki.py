2e624dc306260507c3ba5e3d7cddbc197c6304c1
Viraj Bangari <Viraj3f@gmail.com>
2016-03-05 12:02:49 -0500

    made it into a nicer class

--- wiki.py
+++ wiki.py
@@ -1,29 +1,38 @@
 from ClarifaiParse import ClarifaiParse
 import pywikibot
+import json
+
+class WikiParse:
+    def __init__(self):
+        app_id = "aoaEzeM8d6fiL2L1eX--OtjXFAaPe_CDo6zFvEJD"
+        app_secret = "fjhnJKSdHcZJACroOvCCoNf4tgi9YlOQpi52z-Pb"
+        self.c = ClarifaiParse(app_id, app_secret)
+
+    def get_matches(self, name="earthworm.jpg"):
+        matches = []
+        site = pywikibot.Site()
+        
+        for result in self.c.parse(name):
+            name = result["tag"]
+            try:
+                page = pywikibot.Page(site, name)
+                t = page.get()
+                if "taxobox" in t.lower() or "speciesbox" in t.lower():
+                    print(name, "is found")
+                    result["wiki"] = t
+                    matches.append(t)
+            except pywikibot.exceptions.NoPage:
+                print(name, "page does not exist")
+                continue
+            except pywikibot.exceptions.IsRedirectPage:
+                print(name, "is a redirect page")
+                continue
+            
+        return json.dumps(matches)
 
 
-def run():
-    app_id = "aoaEzeM8d6fiL2L1eX--OtjXFAaPe_CDo6zFvEJD"
-    app_secret = "fjhnJKSdHcZJACroOvCCoNf4tgi9YlOQpi52z-Pb"
-    c = ClarifaiParse(app_id, app_secret)
-    
-    site = pywikibot.Site()
+if __name__ == "__main__":
+    w = WikiParse()
+    url = "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/gray-wolf-snow.jpg.adapt.945.1.jpg"
+    matches = w.get_matches(url)
 
-
-    for result in c.parse("earthworm.jpg"):
-        name = result["tag"]
-        try:
-            page = pywikibot.Page(site, name)
-            s = page.title
-            t = page.get()
-            print(result, "{{Taxobox" in t or "{{speciesbox" in t)
-        except pywikibot.exceptions.NoPage:
-            print(name, "page does not exist")
-            continue
-        except pywikibot.exceptions.IsRedirectPage:
-            print(name, "is a redirect page")
-            continue
-
-
-
-run()
